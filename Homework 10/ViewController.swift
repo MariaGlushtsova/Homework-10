a17e0d1c05b18045e4f7265ca78396ce2eb85982
MariaGlushtsova <glushtsova@tut.by>
2023-05-30 00:50:12 +0300

    delete_all_camments
    Удалены все закоментированные строки

--- Homework 10/ViewController.swift
+++ Homework 10/ViewController.swift
@@ -1,9 +1,3 @@
-//
-//  ViewController.swift
-//  Homework 10
-//
-//  Created by Admin on 30.05.23.
-//
 
 import UIKit
 
@@ -11,7 +5,6 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
 
 
